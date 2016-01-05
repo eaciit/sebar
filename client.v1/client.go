8cf1052cc46d2d4d0cefe94dbcc51dd76e6ae0c6
Arief Darmawan <ariefda@hotmail.com>
2016-01-06 05:23:34 +0700

    update test with write and get test

--- client.v1/client.go
+++ client.v1/client.go
@@ -1,12 +1,33 @@
 package sebarclient
 
+import (
+	//"github.com/eaciit/toolkit"
+	"github.com/eaciit/appserver/v1"
+	"github.com/eaciit/errorlib"
+)
+
+const (
+	objectName = "sebarclient"
+	modClient  = "Client"
+)
+
+var e error
+
 type Client struct {
 	Host, UserID, Secret string
+
+	clientRpc *appserver.Client
 }
 
 func (c *Client) Connect() error {
+	c.clientRpc = new(appserver.Client)
+	e = c.clientRpc.Connect(c.Host, c.Secret, c.UserID)
+	if e != nil {
+		return errorlib.Error(objectName, modClient, "Connect", e.Error())
+	}
 	return nil
 }
 
 func (c *Client) Close() {
+	c.clientRpc.Close()
 }
--- test/z0_test.go
+++ test/z0_test.go
@@ -1,11 +1,14 @@
 package sebar_test
 
 import (
-	"github.com/eaciit/sebar/client.v1"
+	//"github.com/eaciit/sebar/client.v1"
+	"github.com/eaciit/appserver/v1"
+	"github.com/eaciit/crowd"
 	"github.com/eaciit/sebar/v1"
 	"github.com/eaciit/toolkit"
 	//"strconv"
 	"fmt"
+	"strings"
 	"testing"
 	//"time"
 )
@@ -21,7 +24,7 @@ var (
 	nodes       []*sebar.Node
 	session     *sebar.Session
 
-	client *sebarclient.Client
+	client *appserver.Client
 )
 
 func TestMaster(t *testing.T) {
@@ -48,22 +51,74 @@ func TestStorageNode(t *testing.T) {
 	}
 }
 
-func TestStorageSave(t *testing.T) {
-	client = new(sebarclient.Client)
-	client.Host = coordinator.Address
-	client.UserID = userID
-	client.Secret = password
-	e := client.Connect()
+func skipIfClientNil(t *testing.T) {
+	if client == nil {
+		t.Skip()
+	}
+}
+
+func TestClient(t *testing.T) {
+	client = new(appserver.Client)
+	e := client.Connect(coordinator.Address, password, userID)
 	if e != nil {
 		t.Error(e.Error())
 	}
+}
+
+var totalInt int
+
+func TestStorageWrite(t *testing.T) {
+	skipIfClientNil(t)
+	es := []string{}
 
 	toolkit.Printf("Writing Data:\n")
 	for i := 0; i < 200; i++ {
 		dataku := toolkit.RandInt(1000)
-		toolkit.Printf("%d ", dataku)
+		totalInt += dataku
+		//toolkit.Printf("%d ", dataku)
+
+		in := toolkit.M{}.Set("key", fmt.Sprintf("public.dataku.%d", i)).Set("data", dataku)
+		//.Set("encode", false).Set("encoderid", "")
+		writeResult := client.Call("write", in)
+		if writeResult.Status != toolkit.Status_OK {
+			es = append(es, toolkit.Sprintf("Fail to write data %d : %d => %s", i, dataku, writeResult.Message))
+		}
 	}
-	toolkit.Println("")
+
+	if len(es) > 0 {
+		errorTxt := ""
+		if len(es) <= 10 {
+			errorTxt = strings.Join(es, "\n")
+		} else {
+			errorTxt = strings.Join(es[:10], "\n") + "\n... And others ..."
+		}
+		t.Errorf("Write data fail.\n%s", errorTxt)
+	}
+}
+
+func TestStorageGet(t *testing.T) {
+	skipIfClientNil(t)
+
+	in := toolkit.M{}
+	in.Set("key", "dataku")
+	getResult := client.Call("get", in)
+	if getResult.Status != toolkit.Status_OK {
+		t.Errorf(getResult.Message)
+		return
+	}
+
+	data := []int{}
+	e := toolkit.FromBytes(getResult.Data.([]byte), "", &data)
+	if e != nil {
+		t.Errorf("Unable to decode: " + e.Error())
+	}
+	fmt.Println("Result received: %s \n", toolkit.JsonString(data))
+
+	total := int(crowd.From(data).Sum(nil))
+
+	if total != totalInt {
+		t.Errorf("Wrong summation expecting %d got %d", totalInt, total)
+	}
 }
 
 func TestClose(t *testing.T) {
