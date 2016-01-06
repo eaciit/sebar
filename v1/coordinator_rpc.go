a52e88fb1598f15d06ba2e15eb59daca20923782
juragan360 <ariefda@hotmail.com>
2016-01-06 19:35:15 +0700

    update parsekey

--- v1/coordinator_rpc.go
+++ v1/coordinator_rpc.go
@@ -2,14 +2,33 @@ package sebar
 
 import (
 	"github.com/eaciit/toolkit"
+	"strings"
 )
 
 func ParseKey(key string) (owner, table, datakey string) {
+	keyparts := strings.Split(key, ".")
+	if len(keyparts) == 1 {
+		owner = "public"
+		table = "common"
+		datakey = keyparts[0]
+	} else if len(keyparts) == 2 {
+		owner = "public"
+		table = keyparts[0]
+		datakey = keyparts[1]
+	} else if len(keyparts) == 3 {
+		owner = keyparts[0]
+		table = keyparts[1]
+		datakey = keyparts[2]
+	} else if len(keyparts) > 3 {
+		owner = keyparts[0]
+		table = strings.Join(keyparts[1:len(keyparts)-1], ".")
+		datakey = keyparts[len(keyparts)-1]
+	}
 	return
 }
 
-func MakeKey(owner, datakey, dataindex string) string {
-	return owner + "." + datakey + "." + dataindex
+func MakeKey(owner, table, datakey string) string {
+	return owner + "." + table + "." + datakey
 }
 
 func (c *Coordinator) Set(in toolkit.M) *toolkit.Result {
@@ -30,4 +49,8 @@ func (c *Coordinator) Get(in toolkit.M) *toolkit.Result {
 	result := toolkit.NewResult()
 	result.SetErrorTxt("Get command is still under development")
 	return result
+}
+
+func getSmallestNode(o interface{}) int {
+	return 0
 }
